629a226e9891e0aca37a6d7814bb7d30422f1921
SofiaMay26 <tempsofimae@gmail.com>
2024-10-21 17:21:57 +0800

    added first javaScript

---
+++ javaScript.js
@@ -0,0 +1,34 @@
+var form = document.getElementById('form')
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var select = document.getElementById('select').value
+
+
+    switch (select) {
+        case 'Lotion':
+            window.location.href = 'secondPage.html'
+            break;
+
+        case 'Soap':
+            window.location.href = 'secondPage.html'
+            break;
+
+        case 'Perfume':
+            window.location.href = 'secondPage.html'
+            break;
+
+        case 'Hankerchief':
+            window.location.href = 'secondPage.html'
+            break;
+    }
+
+
+})
+
+
+
+
+
+
